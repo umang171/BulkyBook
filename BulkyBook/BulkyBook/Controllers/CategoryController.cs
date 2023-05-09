db8ff8135e03a11fbbca681fd9717bd4e69e87d2
Umang Gohel <gohelumang12@gmail.com>
2023-05-09 16:07:30 +0530

    Added files

---
+++ BulkyBook/BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if ((obj.Name != null) && (obj.DisplayOrder.ToString() != null))
+            {
+                if (obj.Name.Equals(obj.DisplayOrder.ToString()))
+                {
+                    ModelState.AddModelError("SameNameError", "Name can't be same as Display Order");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb= _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if ((obj.Name != null) && (obj.DisplayOrder.ToString() != null))
+            {
+                if (obj.Name.Equals(obj.DisplayOrder.ToString()))
+                {
+                    ModelState.AddModelError("SameNameError", "Name can't be same as Display Order");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["delete"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyBook/BulkyBook/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Display Order")]
+        [Range(1,100,ErrorMessage ="Please enter value between 1 to 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreataedDateTime { get; set; }= DateTime.Now;
+
+    }
+}
