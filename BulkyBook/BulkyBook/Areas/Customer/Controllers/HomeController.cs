cb2344ea0b4f0b9cd90bfb56e7a5e501b33eb267
Umang Gohel <gohelumang12@gmail.com>
2023-05-12 18:16:24 +0530

    Complete product list and details

--- BulkyBook/BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,27 @@ namespace BulkyBook.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork=unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            ShoppingCart shoppingCart = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(prod => prod.Id == id, includeProperties: "Category,CoverType")
+            };
+            return View(shoppingCart);
         }
 
         public IActionResult Privacy()
